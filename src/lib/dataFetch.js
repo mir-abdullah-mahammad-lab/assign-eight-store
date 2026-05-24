d55f11512e9d57766f5e99c62f17e8394d5f07a3
mir-abdullah-mahammad-lab <mirabdullahmahammad@gmail.com>
2026-05-24 21:28:47 +0600

    fixed again

--- src/lib/dataFetch.js
+++ src/lib/dataFetch.js
@@ -1,5 +1,7 @@
 export const dataFetched = async () =>{
-    const promise =  await fetch(`${process.env.BETTER_AUTH_URL}/data.json`)
+    const promise =  await fetch(`${process.env.BETTER_AUTH_URL}/data.json`,{
+        cache: "no-store"
+    })
     const data = await promise.json()
     return data
 }
